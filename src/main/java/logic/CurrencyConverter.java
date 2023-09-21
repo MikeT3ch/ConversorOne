e1e20a3c8fd4050187b0aa54986eb5659f961a00
Michael <michaelnunezt@hotmail.com>
2023-09-20 22:51:00 -0500

    Added working converter button for currency, wip for offline converter

--- src/main/java/logic/CurrencyConverter.java
+++ src/main/java/logic/CurrencyConverter.java
@@ -7,20 +7,19 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import com.google.gson.Gson;
 import java.util.Map;
-import java.util.Locale;
 
-public class CurrencyConversion {
-	double inputMoney, outputMoney;
+public class CurrencyConverter {
+
 	// Declaracion del esqueleto de la URL
 	private static final String API_BASE_URL = "http://data.fixer.io/api/";
-	private static final String ACCESS_KEY = "db7d5846e1722ec72bfceb49cd1dfaa7";
+	private static final String ACCESS_KEY = "?access_key=" + "db7d5846e1722ec72bfceb49cd1dfaa7";
 	private static final String ENDPOINT = "latest";
+	private static final String CURRENCIES = "&symbols=" + "COP,USD,EUR,CHIL,ARG";
 	
-	// public String
 	public CurrencyData getCurrencyData() {
 		try {
 			// String con la url
-			String urlString = API_BASE_URL + ENDPOINT + "?access_key=" + ACCESS_KEY + "&symbols=COP,EUR,USD";
+			String urlString = API_BASE_URL + ENDPOINT + ACCESS_KEY + CURRENCIES;
 			
 			// Conversion a URL para acceder a metodos propios de URL
 			URL url = new URL(urlString);
@@ -57,8 +56,10 @@ public class CurrencyConversion {
 		
 	}
 	
+	// Para hacer la conversion llamar a este metodo.
+	
 	public double doConvertion(String from, String to, double amount) {
-		CurrencyConversion converter = new CurrencyConversion();
+		CurrencyConverter converter = new CurrencyConverter();
 		CurrencyData currencyData = converter.getCurrencyData();
 		
 		if (currencyData != null) {
--- src/main/java/visual/MoneyPanel.java
+++ src/main/java/visual/MoneyPanel.java
@@ -9,8 +9,13 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import java.awt.Dimension;
 import javax.swing.LayoutStyle.ComponentPlacement;
+
+import logic.CurrencyConverter;
+
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MoneyPanel extends JPanel {
 	private JTextField txtInputMoney;
@@ -22,13 +27,6 @@ public class MoneyPanel extends JPanel {
 	public MoneyPanel() {
 		setLayout(null);
 		
-		JPanel moneyPane = new JPanel();
-		moneyPane.setBounds(new Rectangle(0, 0, 600, 320));
-		add(moneyPane);
-		
-		JButton btnConvertirMoney = new JButton("Convertir");
-		btnConvertirMoney.setBorderPainted(false);
-		
 		JLabel lblTest = new JLabel("To:");
 		
 		txtInputMoney = new JTextField();
@@ -39,11 +37,32 @@ public class MoneyPanel extends JPanel {
 		textOutMoney.setEditable(false);
 		textOutMoney.setColumns(10);
 		
-		JComboBox combBoxInputMoney = new JComboBox();
+		final JComboBox combBoxInputMoney = new JComboBox();
 		combBoxInputMoney.setModel(new DefaultComboBoxModel(new String[] {"COP", "USD", "EUR", "CHIL", "ARG"}));
 		
-		JComboBox combBoxOutMoney = new JComboBox();
-		combBoxOutMoney.setModel(new DefaultComboBoxModel(new String[] {"USD", "EUR", "CHIL", "ARG"}));
+		final JComboBox combBoxOutMoney = new JComboBox();
+		combBoxOutMoney.setModel(new DefaultComboBoxModel(new String[] {"USD", "EUR", "CHIL", "ARG", "COP"}));
+		
+		JPanel moneyPane = new JPanel();
+		moneyPane.setBounds(new Rectangle(0, 0, 600, 320));
+		add(moneyPane);
+		
+		JButton btnConvertirMoney = new JButton("Convertir");
+		btnConvertirMoney.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String from = String.valueOf(combBoxInputMoney.getSelectedItem());
+				String to = String.valueOf(combBoxOutMoney.getSelectedItem());
+				double amount = Double.parseDouble(txtInputMoney.getText());
+				
+				CurrencyConverter converter = new CurrencyConverter();
+				double answer = converter.doConvertion(from, to, amount);
+				textOutMoney.setText(String.format("%.2f" ,answer));
+			}
+		});
+		btnConvertirMoney.setBorderPainted(false);
+		
+		
+		
 		GroupLayout gl_moneyPane = new GroupLayout(moneyPane);
 		gl_moneyPane.setHorizontalGroup(
 			gl_moneyPane.createParallelGroup(Alignment.LEADING)
