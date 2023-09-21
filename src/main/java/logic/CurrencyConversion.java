56be0d1812f798ede7be9ab5bca410cadb45bac9
Michael <michaelnunezt@hotmail.com>
2023-09-20 20:37:40 -0500

    Added doConvertion method in CurrencyConversion.java

--- src/main/java/logic/CurrencyConversion.java
+++ src/main/java/logic/CurrencyConversion.java
@@ -1,4 +1,5 @@
 package logic;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,6 +10,7 @@ import java.util.Map;
 
 public class CurrencyConversion {
 	double inputMoney, outputMoney;
+	// Declaracion del esqueleto de la URL
 	private static final String API_BASE_URL = "http://data.fixer.io/api/";
 	private static final String ACCESS_KEY = "db7d5846e1722ec72bfceb49cd1dfaa7";
 	private static final String ENDPOINT = "latest";
@@ -16,23 +18,32 @@ public class CurrencyConversion {
 	// public String
 	public CurrencyData getCurrencyData() {
 		try {
+			// String con la url
 			String urlString = API_BASE_URL + ENDPOINT + "?access_key=" + ACCESS_KEY + "&symbols=COP,EUR";
-			URL url = new URL(urlString);
 			
+			// Conversion a URL para acceder a metodos propios de URL
+			URL url = new URL(urlString);
+			// Abrimos conexión
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
 			
+			// Leemos lo que nos da la URL
 			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			StringBuilder response = new StringBuilder();
 			
+			// Constructor de String
+			StringBuilder response = new StringBuilder();
 			String line;
+			
+			// Mientras que line no sea null, une line a response.
 			while ((line = reader.readLine()) != null) {
                 response.append(line);
             }
+			// Cerramos el lector de buffer.
             reader.close();
             
+            // Desconectamos https.
             connection.disconnect();
-            // return response.toString();
+            // Convertimos JSON a String y lo convertimos en un objeto CurrencyData
             Gson gson = new Gson();
             CurrencyData currencyData = gson.fromJson(response.toString(), CurrencyData.class);
 
@@ -44,6 +55,22 @@ public class CurrencyConversion {
 		}
 		
 	}
+	
+	public double doConvertion(String from, String to, double amount) {
+		CurrencyConversion converter = new CurrencyConversion();
+		CurrencyData currencyData = converter.getCurrencyData();
+		
+		if (currencyData != null) {
+			Map<String, Double> rates = currencyData.getRates();
+			double fromCurrency = rates.get(from);
+			double toCurrency = rates.get(to);
+			double convertedAmount = amount / fromCurrency * toCurrency;
+			return convertedAmount;
+		} else {
+			return amount;
+		}
+	}
+	/*
 	public static void main(String[] args) {
         CurrencyConversion converter = new CurrencyConversion();
         CurrencyData currencyData = converter.getCurrencyData();
@@ -62,5 +89,5 @@ public class CurrencyConversion {
         } else {
             System.out.println("Error al obtener los datos.");
         }
-    }
+    } */
 }
